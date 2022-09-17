77fc2e7a5baa707fb7e66fb3dca2ec93a41b1931
suzutsuki110 <113761004+suzutsuki110@users.noreply.github.com>
2022-09-17 20:36:26 +0800

    github update

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("push test！！");
         System.out.println("push test11！！");
         System.out.println("pull test！！");
+        github 提示更新.....
     }
 }
